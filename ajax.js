b2fd73a5d6df027bde0c48c9f1b194d2baf0ca4a
sneha <snehavrgokulam@gmail.com>
2021-07-10 17:58:24 +0530

    First commit

---
+++ ajax.js
@@ -0,0 +1,13 @@
+function ajax(){
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState==4&&this.status==200){
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    }
+
+    xhttp.open("GET","list.json",true);
+    xhttp.send();
+}
